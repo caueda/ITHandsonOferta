5b024b2dc839df42a78522c94130c69e2af794af
Carlos R. Ueda <carlos.ueda@gmail.com>
2022-08-14 06:55:19 -0300

    First commit

---
+++ src/main/java/com/example/ithandsonrh/ItHandsonRhApplication.java
@@ -0,0 +1,13 @@
+package com.example.ithandsonrh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItHandsonRhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItHandsonRhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ithandsonrh/ItHandsonRhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ithandsonrh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItHandsonRhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
